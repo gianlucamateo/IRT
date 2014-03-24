9b8e3627ad3622a82069247fd106c5355b7d9612
gianlucamateo <gianluca.mateo@gmail.com>
2014-03-24 11:40:56 +0100

    Added base structure for inhomogeneity

---
+++ IRL/IRL/IShape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace IRL.Engine
+{
+    interface IShape
+    {        
+        bool isInside(Vector3 r);
+
+        Vector3 getGradient(Vector3 r);
+
+        float getRefractionIndex(Vector3 r);
+    }
+}
