ea35bf2a09b9d89bd4b046ba37ce14481c4c02bb
gianlucamateo <gianluca.mateo@gmail.com>
2014-05-05 15:30:41 +0200

    Prettyfied some classes

--- IRT/IRL/Shape.cs
+++ IRT/IRL/Shape.cs
@@ -14,6 +14,11 @@ namespace IRT.Engine
 		private float attenuation;
 		private bool interact;
 
+		public Vector3 Position
+		{
+			get { return this.Center; }
+		}
+
 		public Shape(Vector3 center, int zIndex = 0, float attenuation = 1f, bool interact = true)
 		{
 			this.Center = center;
@@ -61,7 +66,7 @@ namespace IRT.Engine
 			normal.Normalize();
 
 			spawnRefl = true;
-			spawnRefr=true;
+			spawnRefr = true;
 
 			// Flip normal if incident ray direction is coming from the inside
 			bool fromInside = Vector3.Dot(normal, incident) > 0;
@@ -95,19 +100,19 @@ namespace IRT.Engine
 					refracted = Vector3.Transform(normal, rot);
 				}
 
-				
 
-				Console.WriteLine ("Refracted: {0}", refracted);
+
+				Console.WriteLine("Refracted: {0}", refracted);
 				// TODO: handle TIR
 
 				if (Math.Abs(nIn - outerRefractionIndex) < 0.02)
 				{
 					spawnRefl = false;
-					
+
 				}
-				
+
 				refl = reflectance(thetaIn, nIn, outerRefractionIndex);
-				
+
 				// Compute and return reflected vector
 				normal *= -1;
 				Vector3.Reflect(ref incident, ref normal, out reflected);
@@ -122,7 +127,7 @@ namespace IRT.Engine
 
 				float thetaIn = (float)Math.Acos(Vector3.Dot(tempIncident, normal));
 				float thetaOut = (float)Math.Asin((outerRefractionIndex / nIn) * Math.Sin(thetaIn));
-				
+
 				Vector3 axis = Vector3.Cross(normal, incident);
 
 				axis.Normalize();
@@ -136,7 +141,7 @@ namespace IRT.Engine
 				}
 
 				float critAngle = (float)Math.Asin(nIn / outerRefractionIndex);
-				if (thetaIn>=critAngle)//(thetaOut >= MathHelper.PiOver2 || float.IsNaN(thetaOut))
+				if (thetaIn >= critAngle)//(thetaOut >= MathHelper.PiOver2 || float.IsNaN(thetaOut))
 				{
 					refl = 1f;
 					spawnRefr = false;
@@ -157,7 +162,7 @@ namespace IRT.Engine
 		private float reflectance(float thetaIn, float n1, float n2)
 		{
 			float scaling = 1;
-			
+
 			if (n1 < n2)
 			{
 				float critAngle = (float)Math.Asin(n1 / n2);
@@ -166,15 +171,10 @@ namespace IRT.Engine
 			float theta = thetaIn;
 			float R0 = (n1 - n2) / (n1 + n2);
 			R0 *= R0;
-			float R = R0 + (1f - R0) * (float)Math.Pow((1f - Math.Cos(theta*scaling)), 5);
+			float R = R0 + (1f - R0) * (float)Math.Pow((1f - Math.Cos(theta * scaling)), 5);
 			return R;
 		}
-
-		public Vector3 Position
-		{
-			get { return this.Center; }
-		}
-
+		
 		public float getAttenuation()
 		{
 			return this.attenuation;
